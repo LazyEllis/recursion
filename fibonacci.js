155b59c8648d92e97d1e0a2521c3e625470ad4cb
Emmanuel Danison <117293986+LazyEllis@users.noreply.github.com>
2024-04-24 15:37:30 +0100

    Remove unnecessary coily braces

--- fibonacci.js
+++ fibonacci.js
@@ -2,28 +2,21 @@ const fibs = (number) => {
   let array = [];
 
   for (i = 0; i < number; i++) {
-    if (i === 0) {
-      array.push(0);
-    } else if (i === 1) {
-      array.push(1);
-    } else {
-      array.push(array[i - 1] + array[i - 2]);
-    }
+    if (i === 0) array.push(0);
+    else if (i === 1) array.push(1);
+    else array.push(array[i - 1] + array[i - 2]);
   }
 
   return array;
 };
 
 const fibsRec = (number) => {
-  if (number === 1) {
-    return [0];
-  } else if (number === 2) {
-    return [0, 1];
-  } else {
-    return fibsRec(number - 1).concat([
-      fibsRec(number - 1).pop() + fibsRec(number - 2).pop(),
-    ]);
-  }
+  if (number === 1) return [0];
+  else if (number === 2) return [0, 1];
+  else
+    return fibsRec(number - 1).concat(
+      fibsRec(number - 1).pop() + fibsRec(number - 2).pop()
+    );
 };
 
 console.log(fibs(8));
